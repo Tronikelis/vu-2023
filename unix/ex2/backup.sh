528f24c654cec5bb783d2be46206502d3970c149
Tronikelis <contactdonatas@gmail.com>
2024-02-15 21:39:20 +0200

    comment

--- unix/ex2/backup.sh
+++ unix/ex2/backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Process Management: Create a script that lists all running processes, prompts the user for a process ID, and then kills that process.
+
 filename=$1
 
 withTime="$(basename $filename)_$(date +%s)"
