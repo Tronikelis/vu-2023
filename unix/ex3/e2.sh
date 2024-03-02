3929ec6a551e794004c49d23a8021a7dbca7886c
Tronikelis <contactdonatas@gmail.com>
2024-03-02 15:33:49 +0200

    second oh god

--- unix/ex3/e2.sh
+++ unix/ex3/e2.sh
@@ -4,49 +4,75 @@ PATH=.
 filename=$1
 contents=$(<$filename)
 
-word=""
-words=()
-word_count=()
-
 calc_word_stats() {
-    local word=$1
+    local word=$@
     declare -A count=()
 
+    local i=0
     for ((i = 0; i < ${#word}; i++)); do
-        local char=${word:$i:1}
-        local curr_count=${count[$char]}
+        declare -l char=${word:$i:1}
 
-        if [[ -z $curr_count ]]; then
-            count[$char]=0
+        if [[ ! -v count[$char] ]]; then
+            count[$char]=1
             continue
         fi
 
+        local curr_count=${count[$char]}
         count[$char]=$(($curr_count + 1))
     done
+
+    echo $word
+    local to_print=""
+
+    local i
+    for ((i = 0; i < ${#word}; i++)); do
+        local char=${word:$i:1}
+        to_print+="${count[$char]},"
+    done
+
+    echo $to_print
+
+    return 0
 }
 
 calc_sentence_word_stats() {
-    local words=$1
+    local words=("$@")
 
-    for word in $words; do
+    local word
+    for word in "${words[@]}"; do
         calc_word_stats $word
     done
+
+    return 0
 }
+
+word=""
+words=()
 
 for ((i = 0; i < ${#contents}; i++)); do
     char=${contents:$i:1}
 
-    if [[ $char == "." ]] || [[ $char == "?" ]] || [[ $char == "!" ]]; then
-        calc_sentence_word_stats $words
-        words=()
-        word=""
-        break
-    fi
-
-    if [[ $char != " " ]]; then
+    if [[ $char != " " ]] &&
+        [[ $char != "." ]] &&
+        [[ $char != "?" ]] &&
+        [[ $char != "!" ]]; then
         word+=$char
     else
         words+=($word)
         word=""
     fi
+
+    if [[ $char == "." ]] ||
+        [[ $char == "?" ]] ||
+        [[ $char == "?" ]] ||
+        [[ $(($i + 1)) == ${#contents} ]]; then
+        words+=($word)
+        calc_sentence_word_stats "${words[@]}"
+
+        word=""
+        words=()
+
+        continue
+    fi
+
 done
